e2f8b4b2e803c466c6d2769e5dda63b989397d26
linhkhanhhoang <93673250+linhkhanhhoang@users.noreply.github.com>
2024-03-24 15:51:04 -0400

    added database connection

--- client/components/RightSideBar.jsx
+++ client/components/RightSideBar.jsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function RightSideBar ({ size, song }) {
-    const { user, isAuthenticated } = useAuth0();
+    const { user, isAuthenticated } = useAuth0(); 
     return (
         isAuthenticated && (
         <div>
---
+++ client/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/database');
+// const UserRouter = require('./routes/User');
+// const CourseRouter = require('./routes/Course');
+// const EnrollmentRouter = require('./routes/Enrollment');
+// const PostRouter = require('./routes/Post');
+
+// set up express server
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// set up routes
+// app.use('/user', UserRouter);
+// app.use('/course', CourseRouter);
+// app.use('/enrollment', EnrollmentRouter);
+// app.use('/post', PostRouter);
+
+//connect to port
+const PORT = process.env.PORT || 8080;
+const startServer = async () => {
+  try {
+
+    // Connect to MongoDB
+    await connectDB();
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+    
+  } catch (error) {
+    console.error('Error connecting to MongoDB: ', error);
+    process.exit(1);
+  }
+};
+
+startServer();
